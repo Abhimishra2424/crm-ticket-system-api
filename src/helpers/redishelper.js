e9afb4340ac6caad6f3aef249d279fd5409e4b0f
Abhishek <abmishra700@gmail.com>
2022-01-23 07:40:59 +0530

    added deleteJWt function they delete jwt from redis databasse

--- src/helpers/redishelper.js
+++ src/helpers/redishelper.js
@@ -27,7 +27,16 @@ const getJWT = (key) => {
   });
 };
 
+const deleteJWT = (key) => {
+  try {
+    client.del(key);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 module.exports = {
   setJWT,
   getJWT,
+  deleteJWT
 };
