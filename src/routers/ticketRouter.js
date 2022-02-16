372fc075d312bcfd7dded82f030265c3fa2d190e
Abhishek <abmishra700@gmail.com>
2022-02-16 23:48:24 +0530

    setup for create ticket api

--- src/routers/ticketRouter.js
+++ src/routers/ticketRouter.js
@@ -1,12 +1,30 @@
 const express = require("express");
 const router = express.Router();
 
+router.all("/", (req, res, next) => {
+  // res.json({
+  //   message: "return tciket"
+  // });
 
-router.all("/", (req, res , next) => {
-  res.json({
-    message: "return tciket" 
-  });
+  next();
 });
 
+// work flow
+// 1. create url endpoint
+// 2. receive new ticket date
+// 3. Authorize every req with jwt
+// 4. insert new ticket to db
+// 5. return new ticket to client
+
+router.post("/", (req, res) => {
+  const { subject, sender, message } = req.body;
+
+  // insert new ticket to db
+  
+
+  res.json({
+    message: "return ticket",
+  });
+});
 
 module.exports = router;
